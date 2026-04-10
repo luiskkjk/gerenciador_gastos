20aceee10598fca27b99516944949978f6b0ebbc
luiskkjk <leborges.sousa8@gmail.com>
2026-04-10 15:39:37 -0300

    MINOR: Adição de ruff

--- src/models.py
+++ src/models.py
@@ -1,11 +1,11 @@
 # Aqui estão definidas as classes de modelo para a aplicação, que funcionam como a estrutura para os dados.
 # Cada classe representa uma tabela no banco de dados, e os atributos da classe correspondem às colunas da tabela.
 
-from flask_sqlalchemy import SQLAlchemy
 from db import db
 
+
 class Gastos(db.Model):
-    __tablename__ = 'gastos'
+    __tablename__ = "gastos"
 
     id = db.Column(db.Integer, primary_key=True)
     valor = db.Column(db.Float, nullable=False)
