03aa504774851d266962d2ec9b5b07197c24cc4d
luiskkjk <leborges.sousa8@gmail.com>
2026-05-14 14:07:08 -0300

    Mudança no fluxo de criação de DB

--- src/init_db.py
+++ src/init_db.py
@@ -2,3 +2,4 @@ from src.app import app, db
 
 with app.app_context():
     db.create_all()
+    print("Tabelas criadas com sucesso!")
--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 # Aqui estão definidas as classes de modelo para a aplicação, que funcionam como a estrutura para os dados.
 # Cada classe representa uma tabela no banco de dados, e os atributos da classe correspondem às colunas da tabela.
 
-from db import db
+from src.db import db
 
 
 class Gastos(db.Model):
